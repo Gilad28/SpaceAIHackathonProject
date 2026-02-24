a513617aff2a724acc10cdcc2208182e5d89859c
Gilad28 <heitnergilad@gmail.com>
2026-02-24 09:55:21 -0800

    added back and forth talking and orbit theme

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ load_dotenv()
 client = genai.Client()
 
 while True:
-    message = input("You: ")
-
+    message = input("Astronaut: ")
+    prompt = f"You are a astronaut mental health assistant, keep your responses short and concise. Astronauts will reach out with mental health issues please help them. The user's message is: {message}"
     if message.lower() == "exit":
         break
 
     response = client.models.generate_content(
         model="gemini-3-flash-preview",
-        contents=message
+        contents=prompt
     )
 
     print("AI:", response.text)
